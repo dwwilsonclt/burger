3c3e60a6c0496279b05f5f0fc9b50c7e7265ffa1
Dave Wilson <dwwilsonclt@icloud.com>
2017-05-26 08:53:52 -0400

    5-26 update

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,6 @@
 INSERT INTO burgers (burger_name) VALUES ('Whopper with Cheese');
-INSERT INTO burgers (burger_name) VALUES ('Whopper Junior');
+INSERT INTO burgers (burger_name) VALUES ('Whopper Junio');
 INSERT INTO burgers (burger_name) VALUES ('Big Mac');
-INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
-INSERT INTO burgers (burger_name) VALUES ('Darin's Mighty Vegan burger with salsa because everyone likes to say salsa!');
+INSERT INTO burgers (burger_name) VALUES ('Darin stolen marble rye burger with salsa because everyone likes to say salsa!');
+INSERT INTO burgers (burger_name) VALUES ('Chris 70s throwback burger with hairy alfalfa sprouts!');
+INSERT INTO burgers (burger_name) VALUES ('David burger - quiet and mysterious but sure to please!');
